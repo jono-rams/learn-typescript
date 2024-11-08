5341ab59d289ede2d518dbc597d9dd0999377483
Jonathan Rampersad <rampersad.jonathan@gmail.com>
2024-11-08 19:29:07 -0400

    Making a simple Next App

---
+++ blog/src/app/about/page.tsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <main>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Dolores autem provident suscipit tempora at quaerat similique tempore ratione velit 
+        alias facere quo dolor, eligendi optio esse laudantium laborum deserunt rem.
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam iure error optio? Incidunt dolorum natus, ipsam vitae voluptate sint eum aperiam, similique, 
+        expedita eligendi tempore provident blanditiis unde ipsa suscipit numquam exercitationem in tempora architecto. Dolore quos mollitia nulla iure veniam excepturi minima, provident, 
+        iste tempore consequatur repellendus ut et sequi ea non esse ratione vitae. Optio, doloribus ipsa ab corporis laboriosam nam exercitationem maiores eos magni nihil sint? Eveniet adipisci 
+        laudantium, cum, dolor consequatur aut nostrum consectetur magni ducimus fugit, deleniti pariatur ab vel ea tenetur sed aspernatur quaerat saepe doloremque sunt sequi laboriosam beatae molestias? 
+        Provident, quisquam rerum?
+      </p>
+    </main>
+  );
+}
